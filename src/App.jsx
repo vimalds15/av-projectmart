65f169503f61f82e78095582b92e0baf330b0364
vimalds15 <antovimalands@gmail.com>
2023-04-14 11:18:35 +0530

    added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Wave from "./components/Wave";
+import { routes } from "./routes/routes";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="relative w-screen min-h-screen flex">
-        <div className="bg-[#F2FAFA] flex-grow">kdajflaj</div>
-        <Wave />
-      </div>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </div>
   );
 }
