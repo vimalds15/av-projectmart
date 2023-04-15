5abc9cca42955e85a416151a6165ff4f469b0164
vimalds15 <antovimalands@gmail.com>
2023-04-15 23:39:42 +0530

    configured for dynamic rendering

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'
 
-const ProjectCard = () => {
+const ProjectCard = ({title,image,category,techStack,price}) => {
   const tags = ["React", "Three.js", "3D"];
   const navigate = useNavigate()
+  const techStackArray = techStack.split(",")
+
 
   return (
     <div className="max-w-[250px] bg-blue-50 p-2 rounded-md">
       <img
-        src="https://i.ytimg.com/vi/0fYi8SGA20k/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDWce0k5XmT0oYgy1xdoRsioH_u5A"
+        src={image}
         alt="project"
         className="w-[250px] rounded-md"
       />
       <p className="truncate font-medium mt-2">
-        3D Website with Three.js | React
+        {title}
       </p>
       <p className="text-sm">
-        Category: <span className="font-semibold">Web Development</span>
+        Category: <span className="font-semibold">{category}</span>
       </p>
       <div className="flex mt-2 gap-2">
-        {tags.map((tag) => (
-          <p className="text-sm bg-blue-100 px-2 rounded-md">{tag}</p>
+        {techStackArray.map((idx,tag) => (
+          <p key={idx} className="text-sm bg-blue-100 px-2 rounded-md">{tag}</p>
         ))}
       </div>
       <div className="flex items-center justify-between mt-4">
         <div>
           <p className="text-sm mt-2">
-            Price: <span className="font-semibold text-lg">$1499</span>
+            Price: <span className="font-semibold text-lg">${price}</span>
           </p>
         </div>
         <div>
