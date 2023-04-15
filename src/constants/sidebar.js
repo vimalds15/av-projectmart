b86377198209e93ba5c9e926104ea65b0d89cd74
vimalds15 <antovimalands@gmail.com>
2023-04-15 11:52:44 +0530

    created sidebar constants

---
+++ src/constants/sidebar.js
@@ -0,0 +1,22 @@
+export const sidebarConstant = [
+    {
+        path:"/dashboard",
+        name:"Dashboard"
+    },
+    {
+        path:"/orders",
+        name:"Orders"
+    },
+    {
+        path:"/home",
+        name:"Home"
+    },
+    {
+        path:"/projects",
+        name:"Projects"
+    },
+    {
+        path:"/cryptowallet",
+        name:"Crypto Wallet"
+    },
+]
