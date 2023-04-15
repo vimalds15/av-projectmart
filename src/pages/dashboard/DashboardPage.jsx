73de034d494540e8cea45ad19a8aba5299e4db6b
vimalds15 <antovimalands@gmail.com>
2023-04-15 11:20:51 +0530

    styling changes

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -4,7 +4,7 @@ import RecentProjects from '../../components/RecentProjects'
 
 const DashboardPage = () => {
   return (
-    <div className='p-10'>
+    <div>
       <h2 className='text-gray-400 font-semibold text-lg'>Happy new month, <span className='font-bold text-black'>Vimal</span></h2>
       <p className='text-sm text-gray-500'>Welcome to your dashboard</p>
       <div>
