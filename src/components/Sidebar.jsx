2434769e40aa7708a3b6fc5c6d996e19bfe15b9d
vimalds15 <antovimalands@gmail.com>
2023-04-15 11:28:15 +0530

    some styling changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,8 @@ import { MdOutlineLibraryBooks, MdOutlineDashboard, MdOutlineListAlt, MdOutlineW
 
 const Sidebar = ({handleItemClick,activeItem}) => {
   return (
-    <div className="hidden md:flex flex-col justify-around w-[20%] mt-16 border bg-white border-r-neutral-200 fixed top-0 left-0 h-full z-10">
-          <div className="px-6 flex flex-col gap-4">
+    <div className="hidden md:flex flex-col justify-around w-[20%] mt-16 border bg-white border-r-neutral-200 fixed top-0 left-0 h-[95%] z-10">
+          <div className="px-6 -mt-7 flex flex-col gap-4">
             
             <div
               className={`flex items-center gap-2 py-4 px-2 rounded-lg pl-4 cursor-pointer hover:bg-gray-100 ${activeItem === "Dashboard" && "bg-gray-100" }`}
