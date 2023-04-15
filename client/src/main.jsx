f9295832bd6c4fccf111f5744b8854bbf40b5f4d
vimalds15 <antovimalands@gmail.com>
2023-04-15 23:39:21 +0530

    added store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./services/store";
+import {Provider} from "react-redux"
+import axios from "axios"
+
+axios.defaults.baseURL="http://localhost:5000/"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
